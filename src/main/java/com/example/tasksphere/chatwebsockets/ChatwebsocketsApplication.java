b71398ec94cbfd22d4bd1eacdcc02d3ed41bf559
alfonsolobato <alfonsolobato@xerintel.es>
2024-05-23 10:25:29 +0200

    Changes

--- src/main/java/com/example/tasksphere/chatwebsockets/ChatwebsocketsApplication.java
+++ src/main/java/com/example/tasksphere/chatwebsockets/ChatwebsocketsApplication.java
@@ -1,10 +1,7 @@
 package com.example.tasksphere.chatwebsockets;
 
-import com.example.tasksphere.chatwebsockets.config.DotEnv;
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ChatwebsocketsApplication {
